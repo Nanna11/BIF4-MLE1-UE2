440d7c58d1f68c65fd62fc6842f44c0cca4074bc
unknown <verena.kopf@technikum-wien.at>
2018-03-17 20:47:43 +0100

    correctetd mistake with pad data

--- KNN/KNN/Program.cs
+++ KNN/KNN/Program.cs
@@ -11,30 +11,64 @@ namespace KNN
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("**********************");
+            //Console.WriteLine("       Iris Data      ");
+            //Console.WriteLine("**********************");
             Stopwatch s = new Stopwatch();
+            //s.Start();
+            //knn k = new knn("bezdekIris.data", " ", 4, false, 2.5, ",");
+            //Console.WriteLine("\nTime for initialization: {0}", s.Elapsed);
+            //s.Restart();
+            //k.Test(10, 10);
+            //Console.WriteLine("\nTime for Testpackages: {0}", s.Elapsed);
+            //k.DisplayConfusionMatrix();
+            //s.Restart();
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    k.Classify("5,1 3,5 1,4 0,2", 10);
+            //}
+            //Console.WriteLine("\nTime for 100 classifications: {0}", s.Elapsed);
+            //s.Restart();
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    k.Classify("5,1 3,5 1,4 0,2", 10);
+            //}
+            //Console.WriteLine("\nTime for 1000 classifications: {0}", s.Elapsed);
+            //s.Restart();
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //    k.Classify("5,1 3,5 1,4 0,2", 10);
+            //}
+            //Console.WriteLine("\nTime for 10000 classifications: {0}", s.Elapsed);
+
+
+            Console.WriteLine("\n\n\n**********************");
+            Console.WriteLine("       Wine Data      ");
+            Console.WriteLine("**********************");
+            s = new Stopwatch();
             s.Start();
-            knn k = new knn("winequality-white.csv", ";", 11, true, 2, ".");
+            knn k2 = new knn("winequality-white.csv", ";", 11, true, 2.5, ".");
             Console.WriteLine("\nTime for initialization: {0}", s.Elapsed);
             s.Restart();
-            k.Test(12,10);
+            k2.Test(10,10);
             Console.WriteLine("\nTime for Testpackages: {0}", s.Elapsed);
-            k.DisplayConfusionMatrix();
+            k2.DisplayConfusionMatrix();
             s.Restart();
             for(int i = 0; i < 100; i++)
             {
-                k.Classify("7;0,27;0,36;20,7;0,045;45;170;1,001;3;0,45;8,8", 10);
+                k2.Classify("7;0,27;0,36;20,7;0,045;45;170;1,001;3;0,45;8,8", 10);
             }
             Console.WriteLine("\nTime for 100 classifications: {0}", s.Elapsed);
             s.Restart();
             for (int i = 0; i < 1000; i++)
             {
-                k.Classify("7;0,27;0,36;20,7;0,045;45;170;1,001;3;0,45;8,8", 10);
+                k2.Classify("7;0,27;0,36;20,7;0,045;45;170;1,001;3;0,45;8,8", 10);
             }
             Console.WriteLine("\nTime for 1000 classifications: {0}", s.Elapsed);
             s.Restart();
             for (int i = 0; i < 10000; i++)
             {
-                k.Classify("7;0,27;0,36;20,7;0,045;45;170;1,001;3;0,45;8,8", 10);
+                k2.Classify("7;0,27;0,36;20,7;0,045;45;170;1,001;3;0,45;8,8", 10);
             }
             Console.WriteLine("\nTime for 10000 classifications: {0}", s.Elapsed);
             Console.ReadKey();
